e0ed5730571d35dd5ecd2b41b22af04d292a0b8c
Maksim Rubanets <maksim@example.com>
2024-11-13 19:31:50 +0100

    update settings file

--- laser_tag_admin/settings.py
+++ laser_tag_admin/settings.py
@@ -97,13 +97,6 @@ WSGI_APPLICATION = 'laser_tag_admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
